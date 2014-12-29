ca9b2c51070b08e0fe5ffc3e1666a2f72d87d478
Administrator <837484691@qq.com>
2014-12-29 17:14:44 +0800

    update

---
+++ src/com/hexor/controller/VideoConrollerForNew.java
@@ -0,0 +1,18 @@
+package com.hexor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 14-12-29
+ * Time: 下午1:06
+ * To change this template use File | Settings | File Templates.
+ * 新版本外链视频控制器
+ */
+@Controller
+@RequestMapping(value="videoNew")
+public class VideoConrollerForNew {
+
+}
